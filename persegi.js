760ef98346b4418786ecadac7f7e3722dedf6664
= <=afhamkelas9@gmail.com>
2026-08-12 12:25:36 +0700

    ubah

---
+++ persegi.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")({ sigint: true });
+console.log('==================')
+console.log('KALKULATOR PERSEGI')
+console.log('==================')
+let sisi = Number(prompt('sisi : '))
+let luas = sisi * sisi
+let keliling = 4 * sisi
+//console.log('luas : '+ luas +'cm2')
+//console.log('keliling : '+ keliling +')
+console.log(`
+luas    : ${luas} cm
+keliling : ${keliling} cm`)
+console.log('====done====')
